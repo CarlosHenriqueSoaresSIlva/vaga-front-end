5b0ff0ed0c36783ab8ded3b219f3fbd588f9673e
CarlosHenriqueSoaresSIlva <87345469+CarlosHenriqueSoaresSIlva@users.noreply.github.com>
2021-07-15 21:49:31 -0300

    Add files via upload

---
+++ Desafio Pokemon_teste/apiquelistatodospokemons.js
@@ -0,0 +1,23 @@
+function loadpk() {
+ let url = 'https://pokeapi.co/api/v2/pokemon/890/';
+ fetch(url)
+ .then((response) => {
+ return response.json();
+ })
+
+ .then((data) => {
+  console.clear();
+console.log(data)
+document.getElementById('nome').innerHTML = data ['name'];
+document.getElementById('numero').innerHTML = data ['id'];
+let img = data['sprites']['font_default'];
+document.getElementById('pic').setAttribute('src' , img);
+ })
+ .catch((erro) => {
+console.log("Erro:" + erro);
+
+ });
+}
+
+document.getElementById('brt').onclick = loadpk;
+
